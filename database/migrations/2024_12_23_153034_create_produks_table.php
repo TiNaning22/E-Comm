2932e93d17c0fef61f1afc8d9060810cb078ae7e
sigitnurdian <sigitnurdian18@gmail.com>
2024-12-23 23:10:49 +0700

    migration

---
+++ database/migrations/2024_12_23_153034_create_produks_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('produks', function (Blueprint $table) {
+            $table->id();
+            $table->string('NamaProduk');
+            $table->integer('HargaProduk');
+            $table->integer('StokProduk');
+            $table->string('Deskripsiroduk');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('produks');
+    }
+};
