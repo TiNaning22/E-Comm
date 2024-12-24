c62e3c6e2fa7d3f7307c00cca1fb06a39dd34573
AdiSlamet <adis8796@gmail.com>
2024-12-24 18:24:58 +0700

    adi

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CartController;
 
 Route::get('/', function () {
-    return view('home');
+    return view('home.home');
 });
 
 Route::get('/web', function () {
