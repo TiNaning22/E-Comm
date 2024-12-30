abbe707e361d176c61ac5ae3d3384d7ae9e7b31c
AdiSlamet <adis8796@gmail.com>
2024-12-31 00:47:47 +0700

    Adi

--- routes/web.php
+++ routes/web.php
@@ -1,42 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CartController;
 
 Route::get('/', function () {
-    return view('home.home');
+    return view('welcome');
 });
 
 Route::get('/dashboard', function () {
-    return view('dasboard.data-produk');
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-
-
-Route::get('/cekot', function () {
-    return view('home.checkout');
-});
-
-Route::get('/login', function () {
-    return view('auth.login');
-});
-
-Route::get('/registrasi', function () {
-    return view('auth.registrasi');
-});
-
-
-
-route::resource('/keranjang', CartController::class);
-
-Route::get('/produk', function () {
-    return view('home.produk-utama');
-});
-
-
-// Route::middleware(['auth'])->group(function () {
-//     Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
-//     Route::post('/cart', [CartController::class, 'store'])->name('cart.store');
-//     Route::patch('/cart/{cart}', [CartController::class, 'update'])->name('cart.update');
-//     Route::delete('/cart/{cart}', [CartController::class, 'destroy'])->name('cart.destroy');
-// });
+require __DIR__.'/auth.php';
